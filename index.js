cbc2c3d310c1fd9a639a7336b0bf2dd9510a424f
Jeff <earl_capistrano@dlsu.edu.ph>
2020-09-28 00:16:19 +0800

    test

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ mongoose.connection.once('open', ()=>{
     console.log("MongoDB Connected");
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
     res.set("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0");
     next();
