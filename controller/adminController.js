9d610a6c5e23240347b424b8732d5fd9ff0d9047
Jeff <earl_capistrano@dlsu.edu.ph>
2020-09-28 00:08:06 +0800

    test

--- controller/adminController.js
+++ controller/adminController.js
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
         //     password: admin.password,
         // });
     } else {
-        res.redirect("/login");
+        res.redirect("/admin");
     }
 });
 
